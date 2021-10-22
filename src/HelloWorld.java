2cf749ffefe7696a80f9c6244cb8dd06285a90a9
Bosch Leith <bosch.leith@gmail.com>
2021-10-22 10:42:11 -0500

    completed Java Syntax, Data Types, and Variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,52 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.print("Hello, ");
-        System.out.print("World!");
+//        System.out.print("Hello, ");
+//        System.out.print("World!");
+
+        int myFavoriteNumber = 18;
+        String myString = "Bosch Leith";
+
+//        myString = 'S';
+//        myString = 3.14159;
+
+        long myNumber;
+//        myNumber = 3.14;
+//        myNumber = 123L;
+//        myNumber = 123;
+        myNumber = (long) 3.14;
+
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String class;
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
