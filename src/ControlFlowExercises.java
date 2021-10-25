a05e2de97bd7b1357f34392851910c29cc848c95
Bosch Leith <bosch.leith@gmail.com>
2021-10-25 11:23:49 -0500

    Completed exercise #1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,49 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
 
+//        1. Loop Basics
+//        *** do-while Loops ***
+
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i <= 100);
+
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i <= 1000000);
+
+//        *** Refactored to for Loops ***
+
+//        for (int i = 5; i <= 15; i++){
+//            System.out.println(i);
+//        }
+
+//        for (int i = 0; i <= 100; i+= 2){
+//            System.out.println(i);
+//        }
+
+//        for (int i = 100; i >= -10; i -= 5){
+//            System.out.println(i);
+//        }
+
+//        for (long i = 2; i <= 1000000; i *= i){
+//            System.out.println(i);
+//        }
     }
 }
