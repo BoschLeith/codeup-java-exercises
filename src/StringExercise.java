155520bdacd247d0c8cc67dabe83867ba3e42f57
Bosch Leith <bosch.leith@gmail.com>
2021-10-26 10:47:20 -0500

    Complete String Basics exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,21 @@
 public class StringExercise {
     public static void main(String[] args){
 
+        String stringOne = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(stringOne);
+        System.out.println();
+
+        String stringTwo = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(stringTwo);
+        System.out.println();
+
+        String stringThree = "In windows, the main drive is usually C:\\";
+        System.out.println(stringThree);
+        System.out.println();
+
+        String stringFour = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(stringFour);
     }
 }
