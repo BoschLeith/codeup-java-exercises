057ba42caa59bd1bca71b381a0db4ae21ff80584
Bosch Leith <bosch.leith@gmail.com>
2021-10-26 11:13:31 -0500

    Complete Bob conversation bot exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args){
 
+        Scanner scanner = new Scanner(System.in);
 
+        boolean confirmation;
+
+        do {
+            System.out.print("Ask Bob something: ");
+            String userInput = scanner.next();
+
+            if (userInput.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (userInput.equals("")) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+            System.out.print("Do you want to keep talking to Bob? [Y/N] ");
+            String userConfirmation = scanner.next();
+            confirmation = userConfirmation.equalsIgnoreCase("Y");
+            scanner.nextLine();
+        } while (confirmation);
     }
 }
