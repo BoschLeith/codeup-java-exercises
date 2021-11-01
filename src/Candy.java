c14a16570542165375cb7029bfee4e4dc85cc64e
Bosch Leith <bosch.leith@gmail.com>
2021-11-01 09:48:24 -0500

    Added warmup instructions

--- src/Candy.java
+++ src/Candy.java
@@ -1,5 +1,14 @@
 // Mon Nov 1 WARM-UP
 
+//Java WarmUp
+//		Write a public class named ‘Candy’ - this class should have two public fields, both taking in a string literal as a value.
+//		The first would be ‘name’ and the second will be ‘size’.
+//		The class will also need two constructors - the first for if we know the name, but not the size. Your first constructor should set some kind of string literal for the size.
+//		The second would be for if we know both the name and the size. This second constructor will pass through those arguments into the Candy object being instantiated.
+//		BONUS:
+//		Write a method for your Candy class - this method should print out a message saying:
+//		“Ahh, a [name]! I see that you got the [size] of it. Sounds good! :)”
+
 public class Candy {
 		public String name;
 		public String size;
