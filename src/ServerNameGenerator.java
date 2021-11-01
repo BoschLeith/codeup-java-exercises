a33bdaa117ce82f5044e081509bfe81c2f043d39
Bosch Leith <bosch.leith@gmail.com>
2021-11-01 12:42:04 -0500

    Complete ServerNameGenerator.java exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,25 @@
 public class ServerNameGenerator {
+	static String[] nouns = {"cat", "dog", "bear", "bat", "duck"};
+	static String[] adjectives = {"big", "small", "short", "tall", "fat"};
+
+
+	static String randomElement(String[] array){
+
+		int min = 1;
+		int max = array.length;
+		int rand = (int)(Math.random() * (max - min + 1));
+		String randElement = "";
+
+		for (int i = 0; i < 1; i++){
+			randElement = array[rand];
+		}
+		return randElement;
+	}
+
+	public static void main(String[] args) {
+		String randomNoun = randomElement(nouns);
+		String randomAdj = randomElement(adjectives);
+
+		System.out.printf("Here is your server name:\n%s-%s", randomNoun, randomAdj);
+	}
 }
