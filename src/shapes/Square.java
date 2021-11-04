8202bcf34fb619a0f1d2941659fc48c83dc5f108
Bosch Leith <bosch.leith@gmail.com>
2021-11-04 14:20:49 -0500

    Square class now extends Quadrilateral

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,18 +1,42 @@
 package shapes;
 
-public class Square extends Rectangle{
-	protected int side;
+public class Square extends Quadrilateral{
 
-	public Square(int side) {
+	public Square(double side) {
 		super(side, side);
-		this.side = side;
 	}
 
-	public int getArea(){
-		return side * side;
+	@Override
+	public double getPerimeter() {
+		return length * 4;
 	}
 
-	public int getPerimeter(){
-		return side * 4;
+	@Override
+	public double getArea() {
+		return length * length;
 	}
+
+	@Override
+	public void setLength(double side) {
+		this.length = side;
+	}
+
+	@Override
+	public void setWidth(double side) {
+		this.width = side;
+	}
+//	protected int side;
+//
+//	public Square(int side) {
+//		super(side, side);
+//		this.side = side;
+//	}
+//
+//	public int getArea(){
+//		return side * side;
+//	}
+//
+//	public int getPerimeter(){
+//		return side * 4;
+//	}
 }
