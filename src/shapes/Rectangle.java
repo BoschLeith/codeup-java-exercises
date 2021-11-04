f00354d09f3cdcfe902e00502febd8c956c399f8
Bosch Leith <bosch.leith@gmail.com>
2021-11-04 14:20:21 -0500

    Rectangle class now extends Quadrilateral

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,19 +1,42 @@
 package shapes;
 
-public class Rectangle {
-	protected int length;
-	protected int width;
+public class Rectangle extends Quadrilateral {
+	public Rectangle(int length, int width) {
+		super(length, width);
+	}
 
-	public Rectangle(int length, int width){
+	@Override
+	public void setLength(double length) {
 		this.length = length;
+	}
+
+	@Override
+	public void setWidth(double width) {
 		this.width = width;
 	}
 
-	public int getArea(){
-		return this.length * this.width;
-	}
-
-	public int getPerimeter(){
+	@Override
+	public double getPerimeter() {
 		return (this.length * 2) + (this.width * 2);
 	}
+
+	@Override
+	public double getArea() {
+		return this.length * this.width;
+	}
+//	protected int length;
+//	protected int width;
+//
+//	public Rectangle(int length, int width){
+//		this.length = length;
+//		this.width = width;
+//	}
+//
+//	public int getArea(){
+//		return this.length * this.width;
+//	}
+//
+//	public int getPerimeter(){
+//		return (this.length * 2) + (this.width * 2);
+//	}
 }
