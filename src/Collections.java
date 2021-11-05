908d1d1d65a83ef2c9607ac8b3f301615ce792ac
Bosch Leith <bosch.leith@gmail.com>
2021-11-05 10:51:34 -0500

    Added HashMap lecture notes

--- src/Collections.java
+++ src/Collections.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Collections {
 	public static void main(String[] args) {
+
+		// ArrayList Lecture Notes
 		ArrayList<String> students = new ArrayList<>();
 
 		students.add("Bosch");
@@ -19,5 +22,32 @@ public class Collections {
 		System.out.println("students.isEmpty() = " + students.isEmpty());
 		System.out.println("students.remove(\"Shanshan\") = " + students.remove("Shanshan"));
 		System.out.println(students);
+
+		// HashMap Lecture Notes
+		HashMap<String, String> usernames = new HashMap<>();
+
+		usernames.put("Ryan", "Ryan01");
+		usernames.put("Zach", "Zach02");
+		usernames.put("Fernando", "Fernando03");
+		usernames.put("Justin", "Justin04");
+
+		System.out.println();
+		System.out.println(usernames);
+		System.out.println("usernames.get(\"Zach\") = " + usernames.get("Zach"));
+		System.out.println("usernames.getOrDefault(\"Alex\", \"No username found\") = " + usernames.getOrDefault("Alex", "No username found"));
+		System.out.println("usernames.containsKey(\"Ryan\") = " + usernames.containsKey("Ryan"));
+		System.out.println("usernames.containsValue(\"Fernando03\") = " + usernames.containsValue("Fernando03"));
+
+		System.out.println();
+		usernames.putIfAbsent("Bosch", "Bosch05");
+		usernames.putIfAbsent("Ryan", "Ryan01");
+		System.out.println(usernames);
+		usernames.remove("Fernando");
+		System.out.println(usernames);
+		usernames.replace("Justin", "Justin004");
+		System.out.println(usernames);
+		usernames.clear();
+		System.out.println("usernames = " + usernames);
+		System.out.println("usernames.isEmpty() = " + usernames.isEmpty());
 	}
 }
