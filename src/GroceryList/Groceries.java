adaa0c0d1ddda1d02f4fa414b3b7ad9c162f2f9b
Bosch Leith <bosch.leith@gmail.com>
2021-11-05 16:07:56 -0500

    Added do/while loop and display final grocery list alphabetically

--- src/GroceryList/Groceries.java
+++ src/GroceryList/Groceries.java
@@ -4,6 +4,7 @@ import util.Input;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Groceries {
 
@@ -32,8 +33,17 @@ public class Groceries {
 		System.out.println(Category);
 	}
 
+	// Gets all items in a category and displays "items x quantity" alphabetically
+	public static void sortCategoryItems(HashMap<String, Integer> Category) {
+		TreeMap<String, Integer> sorted = new TreeMap<>(Category);
+		for (Map.Entry<String, Integer> entry : sorted.entrySet())
+			System.out.println(entry.getKey() + " x " + entry.getValue());
+	}
+
 	public static void GroceryList(){
 		Input input = new Input();
+
+		boolean confirmation;
 
 		HashMap<String, Integer> Dairy = new HashMap<>();
 		HashMap<String, Integer> Produce = new HashMap<>();
@@ -41,23 +51,34 @@ public class Groceries {
 		HashMap<String, Integer> Frozen = new HashMap<>();
 		HashMap<String, Integer> Pantry = new HashMap<>();
 
-		System.out.println("1. Dairy\n2. Produce\n3. Deli\n4. Frozen\n5. Pantry\n");
-		System.out.print("Pick a category: ");
-		int usersCategory = input.getInt();
+		do {
+			System.out.println("1. Dairy\n2. Produce\n3. Deli\n4. Frozen\n5. Pantry\n");
+			System.out.print("Pick a category: ");
+			int usersCategory = input.getInt();
 
-		if (usersCategory == 1){
-			addUserItem(Dairy, "Dairy", input);
-		} else if (usersCategory == 2){
-			addUserItem(Produce, "Produce", input);
-		} else if (usersCategory == 3){
-			addUserItem(Deli, "Deli", input);
-		} else if (usersCategory == 4){
-			addUserItem(Frozen, "Frozen", input);
-		} else if (usersCategory == 5){
-			addUserItem(Pantry, "Pantry", input);
-		} else {
-			GroceryList();
-		}
+			if (usersCategory == 1){
+				addUserItem(Dairy, "Dairy", input);
+			} else if (usersCategory == 2){
+				addUserItem(Produce, "Produce", input);
+			} else if (usersCategory == 3){
+				addUserItem(Deli, "Deli", input);
+			} else if (usersCategory == 4){
+				addUserItem(Frozen, "Frozen", input);
+			} else if (usersCategory == 5){
+				addUserItem(Pantry, "Pantry", input);
+			} else {
+				GroceryList();
+			}
+			System.out.println("Would you like to:\n1. Add another item to your list \nor\n2. Finalize your grocery list?\nENTER [1 or 2]");
+			String userConfirmation = input.getString();
+			confirmation = userConfirmation.equals("1");
+		} while (confirmation);
+		System.out.println("Your final grocery list is: ");
+		sortCategoryItems(Dairy);
+		sortCategoryItems(Produce);
+		sortCategoryItems(Deli);
+		sortCategoryItems(Frozen);
+		sortCategoryItems(Pantry);
 	}
 
 	public static void main(String[] args) {
