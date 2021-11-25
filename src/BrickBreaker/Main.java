1f32e50fb22e508d6539ab9d8a9f7dc80185b0c4
Bosch Leith <bosch.leith@gmail.com>
2021-11-24 20:03:10 -0600

    Changed: put obj.setVisible(true); after obj.add(gamePlay); This will make it visible after the objects are drawn

--- src/BrickBreaker/Main.java
+++ src/BrickBreaker/Main.java
@@ -13,8 +13,8 @@ public class Main {
 		obj.setBounds(10,10,700,600);
 		obj.setTitle("BrickBreaker");
 		obj.setResizable(false);
-		obj.setVisible(true);
 		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		obj.add(gamePlay);
+		obj.setVisible(true);
 	}
 }
